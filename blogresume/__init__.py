f70e8e1daa54091abba9a4d56b193f4d0e822624
Wayne Bryant <waynecbryant@gmail.com>
2018-12-09 21:18:35 -0500

    Fully functional blog website as per Flask Bootcamp.

--- blogresume/__init__.py
+++ blogresume/__init__.py
@@ -29,9 +29,12 @@ login_manager.init_app(app)
 login_manager.login_view = 'users.login'
 #Registered Pages
 from blogresume.core.views import core
-from blogresume.error_pages.handlers import error_pages
 from blogresume.users.views import users
+from blogresume.blog_posts.views import blog_posts
+from blogresume.error_pages.handlers import error_pages
+
 
 app.register_blueprint(core)
-app.register_blueprint(error_pages)
 app.register_blueprint(users)
+app.register_blueprint(blog_posts)
+app.register_blueprint(error_pages)
--- blogresume/blog_posts/forms.py
+++ blogresume/blog_posts/forms.py
@@ -1,1 +1,9 @@
-from flask import Flask
+#blog_posts/forms.py
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+class BlogPostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    text = StringField('Text', validators=[DataRequired()])
+    submit = SubmitField('Post')
